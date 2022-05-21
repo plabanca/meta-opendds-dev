30c164d4011aff11165172588c0e8832af62b86f
Phillip LaBanca <labancap@objectcomputing.com>
2022-05-21 01:20:57 -0500

    Added streamlined yocto_check_layer.yml and script

---
+++ .github/scripts/yocto-check-layer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Yocto Release Branch $YOCTO_RELEASE"
+echo "Temp Dir: $RUNNER_TEMP"
+echo "Workspace: $GITHUB_WORKSPACE"
+
+cd $RUNNER_TEMP
+git clone --depth 1 --branch $YOCTO_RELEASE https://git.yoctoproject.org/git/poky 
+. ./poky/oe-init-build-env build
+yocto-check-layer --with-software-layer-signature-check --debug "$GITHUB_WORKSPACE"
